e59f690f11ca4ff9642103bbf7d67967524d30ee
pk1260 <43113381+pk1260@users.noreply.github.com>
2019-07-03 21:48:05 +0200

    Add files via upload

--- ajax5/getData.php
+++ ajax5/getData.php
@@ -6,33 +6,34 @@
  * Time: 11:41
  */
 
-  include "dbVars.php"; // inlog gegevens
-  $resultArray = array(); // array voor de query output
-  $search = $_GET['q']; // no sql protection
-  $type = $_GET['type']; // no sql protection
-  $con = mysqli_connect($servername,$uid,$pwd,$database); // bereid connectie
-  if(!$con) {
+include "dbVars.php"; // inlog gegevens
+$resultArray = array(); // array voor de query output
+$search = $_GET['q']; // no sql protection
+$type = $_GET['type']; // no sql protection
+$con = mysqli_connect($servername, $uid, $pwd, $database); // bereid connectie
+if (!$con) {
     die('Could not connect: ' . mysqli_error($con));
-  }
-  mysqli_select_db($con, "world"); // selecteer database
-  $sql="SELECT * FROM country WHERE name LIKE '$search%' ";
-  if ($type == "list") {
+}
+$con->set_charset("utf8");
+mysqli_select_db($con, "world"); // selecteer database
+$sql = "SELECT * FROM country WHERE name LIKE '$search%' ";
+if ($type == "list") {
     $result = mysqli_query($con, $sql);
     while ($row = mysqli_fetch_array($result)) {
-        $resultArray[]=$row['Name'];
+        $resultArray[] = $row['Name'];
     }
     echo json_encode($resultArray);//maak van array een JSON gecodeerde string
-  }
-  if ($type == "answer") {
+}
+if ($type == "answer") {
 
-     $result = mysqli_query($con, $sql);
+    $result = mysqli_query($con, $sql);
     //print_r($result);
     while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-        $resultArray[]=$row;
-       print_r($resultArray);//werkt
-     echo json_encode($resultArray, JSON_UNESCAPED_UNICODE);//maak van array een JSON gecodeerde string
+        $resultArray[] = $row;
+        //print_r($resultArray);//werkt
+        echo json_encode($resultArray);//maak van array een JSON gecodeerde string
     }
 
-  }
-  mysqli_close($con);
+}
+mysqli_close($con);
 ?>
